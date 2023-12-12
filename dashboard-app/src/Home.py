c4746efc88b0cb1b93a890fba474ead3b764b0a2
Orçun Birgül <obirgul@users.noreply.github.com>
2023-12-12 23:41:19 +0300

    update

--- dashboard-app/src/Home.py
+++ dashboard-app/src/Home.py
@@ -6,7 +6,13 @@ st.set_page_config(page_title="Electicity Dashboard", page_icon="⚡️", layout
 def home():
     st.title("Insight Engine")
 
-    st.write("""This is a sample dashboard for Electricity Data.""")
+    st.write("""Welcome to this sample dashboard! This dashboard serves as an example and is suitable for 
+    introductory purposes. The primary focus of this project is to highlight good practices in data pipeline 
+    development. As a result, advanced functionalities were intentionally taken out, and only basic graphs were 
+    incorporated.
+
+Feel free to explore and use this dashboard as a starting point for understanding data pipeline structures and best 
+practices.""")
 
 
 if __name__ == '__main__':
